0e942d7980e08e04daa684cdcb3e11db968e11e5
Rob Weinhandl <robweinhandl@gmail.com>
2017-05-01 11:53:59 -0500

    Refactor block cache logic out of constructor
    This commit moves block cache logic out the constructor, and into their
    specific block class methods. In practice, some method calls made would
    error out in FPC mode. Moving that logic out allows the class to be
    instantiated in FPC mode, while perserving caching logic.

--- app/code/community/Turnto/Client/Block/Checkoutchatterpdp.php
+++ app/code/community/Turnto/Client/Block/Checkoutchatterpdp.php
@@ -5,26 +5,27 @@ class Turnto_Client_Block_Checkoutchatterpdp extends Mage_Core_Block_Template
     const TURNTO_CC_PDP_CACHE_TAG = 'TURNTO_CC_PDP_CACHE_TAG';
     const TURNTO_CC_PDP_CACHE_KEY = 'TURNTO_CC_PDP_CACHE_KEY';
 
-    public function __construct()
+    public function getCacheKey()
     {
-        // cache by store, http(s), package, theme
-        $this->addData(array(
-            // defaults to 15 minutes
-            'cache_lifetime' => Mage::getStoreConfig('turnto_admin/general/cc_display_cache_time') ? intval(Mage::getStoreConfig('turnto_admin/general/cc_display_cache_time')) : 900,
-            'cache_tags' => array(
-                $this::TURNTO_CC_PDP_CACHE_TAG,
-                Mage_Catalog_Model_Product::CACHE_TAG,
-                Mage::app()->getStore()->getId(),
-                (int)Mage::app()->getStore()->isCurrentlySecure(),
-                Mage::getDesign()->getPackageName(),
-                Mage::getDesign()->getTheme('template')
-            ),
-            'cache_key' => $this::TURNTO_CC_PDP_CACHE_KEY . $this->getProduct()->getId()
-        ));
+        $helper = Mage::helper('turnto_client_helper/data');
+        return $this::TURNTO_CC_PDP_CACHE_KEY . $helper->getProduct()->getId();
     }
 
-    public function getProduct()
+    public function getCacheLifetime()
     {
-        return Mage::registry('current_product');
+        return Mage::getStoreConfig('turnto_admin/general/cc_display_cache_time') ? intval(Mage::getStoreConfig('turnto_admin/general/cc_display_cache_time')) : 900;
     }
+
+    public function getCacheTags()
+    {
+        return array(
+            $this::TURNTO_CC_PDP_CACHE_TAG,
+            Mage_Catalog_Model_Product::CACHE_TAG,
+            Mage::app()->getStore()->getId(),
+            (int)Mage::app()->getStore()->isCurrentlySecure(),
+            Mage::getDesign()->getPackageName(),
+            Mage::getDesign()->getTheme('template')
+        );
+    }    
+
 }
--- app/code/community/Turnto/Client/Block/Qateaser.php
+++ app/code/community/Turnto/Client/Block/Qateaser.php
@@ -5,21 +5,35 @@ class Turnto_Client_Block_Qateaser extends Mage_Core_Block_Template
     const TURNTO_QA_TEASERS_CACHE_TAG = 'TURNTO_QA_TEASERS_CACHE_TAG';
     const TURNTO_QA_TEASERS_CACHE_KEY = 'TURNTO_QA_TEASERS_CACHE_KEY';
 
-    public function __construct()
+    public function getCacheKey(){
+        return $this::TURNTO_QA_TEASERS_CACHE_KEY;
+    }
+
+    public function getCacheKeyInfo()
     {
-        // cache by store, http(s), package, theme
-        $this->addData(array(
-            // defaults to 15 minutes
-            'cache_lifetime' => Mage::getStoreConfig('turnto_admin/general/teaser_cache_time') ? intval(Mage::getStoreConfig('turnto_admin/general/teaser_cache_time')) : 900,
-            'cache_tags' => array(
-                $this::TURNTO_QA_TEASERS_CACHE_TAG,
-                Mage_Catalog_Model_Product::CACHE_TAG,
-                Mage::app()->getStore()->getId(),
-                (int)Mage::app()->getStore()->isCurrentlySecure(),
-                Mage::getDesign()->getPackageName(),
-                Mage::getDesign()->getTheme('template')
-            ),
-            'cache_key' => $this::TURNTO_QA_TEASERS_CACHE_KEY
-        ));
+        $helper = Mage::helper('turnto_client_helper/data');
+        $info = parent::getCacheKeyInfo();
+        if ($helper->getProduct())
+        {
+            $info['product_id'] = $helper->getProduct()->getId();
+        }
+        return $info;
+    }
+
+    public function getCacheLifetime()
+    {
+        return Mage::getStoreConfig('turnto_admin/general/teaser_cache_time') ? intval(Mage::getStoreConfig('turnto_admin/general/teaser_cache_time')) : 900;
+    }
+
+    public function getCacheTags()
+    {
+        return array(
+            $this::TURNTO_QA_TEASERS_CACHE_TAG,
+            Mage_Catalog_Model_Product::CACHE_TAG,
+            Mage::app()->getStore()->getId(),
+            (int)Mage::app()->getStore()->isCurrentlySecure(),
+            Mage::getDesign()->getPackageName(),
+            Mage::getDesign()->getTheme('template')
+        );
     }
 }
